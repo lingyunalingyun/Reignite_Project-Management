cb11da3b834f04396374fc4c8bb4ef36f780f351
lingyunalingyun <66152556+lingyunalingyun@users.noreply.github.com>
2026-09-15 20:09:15 +0800

    feat: 增加perperty属性

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-slint::include_modules!();  //将 build.rs 中编译的 slint 文件包含进来
+slint::include_modules!();  // 将 build.rs 中编译的 slint 文件包含进来
 
 fn main() -> Result<(), slint::PlatformError> {
     let window = MainWindow::new()?;  //创建一个 MainWindow 实例
