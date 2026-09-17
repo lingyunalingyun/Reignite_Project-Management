04589cfb66789a9be3c72478b1f295e22b7eb2c6
dunianxue <dunianxueqiaobian@gmail.com>
2026-09-17 20:45:29 +0800

    feat : 新增两种排序方式且时间为默认排序

--- src/core/scanner.rs
+++ src/core/scanner.rs
@@ -4,16 +4,26 @@ use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
 
-pub struct FileInfo{                               //定义文件和修改时间的结构体
-    path: String,
-    modified: SystemTime,
+pub enum SortMethod {
+    ModifiedTimeAsc,    // 修改时间升序
+    FileSizeDesc,       // 文件大小降序
+    ModifyCountDesc,    // 修改次数降序
+}
+
+pub struct FileInfo {                               //定义文件和修改时间的结构体
+    pub path: String,
+    pub modified: SystemTime,
+    pub size: u64,
+    pub modify_count: u32,
 }
 
 
 
-fn get_modified_time(path:&path)->Option<SystemTime>{                     //获取文件修改时间函数
-    let metadata=fs::metadata(path).ok()?;
-    metadata.modified().ok()
+pub fn get_file_info(path: &Path) -> Option<(SystemTime, u64)> {              //获取文件信息函数，返回修改时间和大小
+    let metadata = fs::metadata(path).ok()?;
+    let modified = metadata.modified().ok()?;
+    let size = metadata.len();
+    Some((modified, size))
 }
 
 
@@ -35,10 +45,12 @@ fn scan_dir(dir: &Path, results: &mut Vec<FileInfo>) {                       //�
             scan_dir(&path, results);
         } else if path.is_file() {
                                              // 是文件，获取修改时间并保存
-            if let Some(modified) = get_modified_time(&path) {
+            if let Some((modified, size)) = get_file_info(&path) {
                 results.push(FileInfo {
                     path: path.to_string_lossy().to_string(),
                     modified,
+                    size,
+                    modify_count: 0,  // 修改次数暂时用0占位
                 });
             }
         }
@@ -47,16 +59,28 @@ fn scan_dir(dir: &Path, results: &mut Vec<FileInfo>) {                       //�
 
 
 
-fn sort_by_modified(files: &mut Vec<FileInfo>) {                       //排序函数
-    files.sort_by(|a, b| a.modified.cmp(&b.modified));
+fn sort_files(files: &mut Vec<FileInfo>, method: &SortMethod) {                  //排序函数
+    match method {
+        SortMethod::ModifiedTimeAsc => {
+            files.sort_by(|a, b| a.modified.cmp(&b.modified));
+        }
+        SortMethod::FileSizeDesc => {
+            files.sort_by(|a, b| b.size.cmp(&a.size));
+        }
+        SortMethod::ModifyCountDesc => {
+            // 修改次数排序暂时按0占位，后续实现
+            files.sort_by(|a, b| b.modify_count.cmp(&a.modify_count));
+        }
+    }
 }
 
 
 
-pub fn scan_and_sort(dir: &Path) -> Vec<FileInfo> {              //对外公开的入口函数
+pub fn scan_and_sort(dir: &Path, method: Option<SortMethod>) -> Vec<FileInfo> {              //对外公开的入口函数
     let mut files = Vec::new();
     scan_dir(dir, &mut files);
-    sort_by_modified(&mut files);
+    let method = method.unwrap_or(SortMethod::ModifiedTimeAsc);
+    sort_files(&mut files, &method);
     files
 }
 
